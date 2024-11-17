308bc8e5a49b8bb5f1fe659da23867c00adfbfea
Murilo Chiquetti Galina <murilochiquettigalina@MacBook-Air-de-Murilo.local>
2024-11-17 01:19:23 -0300

    feat: Adding basic websocket route.

--- main.go
+++ main.go
@@ -1,6 +1,9 @@
 package main
 
 import (
+	"log"
+
+	"github.com/gofiber/contrib/websocket"
 	"github.com/gofiber/fiber/v2"
 	"github.com/gofiber/fiber/v2/middleware/cors"
 )
@@ -8,12 +11,35 @@ import (
 func main() {
 	app := fiber.New()
 
-	app.Use(cors.New())
-
-	app.Get("/", func(c *fiber.Ctx) error {
-		return c.SendString("Hello World!")
+	app.Use("/chat", func(c *fiber.Ctx) error {
+		if websocket.IsWebSocketUpgrade(c) {
+			c.Locals("allowed", true)
+			return c.Next()
+		}
+		return fiber.ErrUpgradeRequired
 	})
 
+	app.Get("/chat", websocket.New(func(c *websocket.Conn) {
+		var (
+			mt  int
+			msg []byte
+			err error
+		)
+		for {
+			if mt, msg, err = c.ReadMessage(); err != nil {
+				log.Println("read:", err)
+				break
+			}
+			log.Printf("message: %s", msg)
+
+			if err = c.WriteMessage(mt, msg); err != nil {
+				log.Println("write:", err)
+				break
+			}
+		}
+	}))
+
+	app.Use(cors.New())
 	app.Listen(":8080")
 
 }
